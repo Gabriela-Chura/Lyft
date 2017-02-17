4e4a63c71ec78bb199a6feab052fb744ce67953d
Daniela Alvarez Zegarra <daniela.alvarez04@gmail.com>
2017-02-17 10:47:14 -0500

    input number

--- JS/app.js
+++ JS/app.js
@@ -1,23 +1,48 @@
-function init(){
-	
+var map;
+var latlong={
+    lat: -16.457389199999998,
+    lng: -71.5315308
+}
+var opcionesMapa = {
+    enableHighAccuracy: true
 }
 
-
-
-
-var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), 
-	{
-    center: 
-	  {lat: -16.457389199999998, 
-	   lng: -71.5315308},
-    zoom: 12
-  }
-);
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: latlong.lat,
+            lng: latlong.lng},
+            zoom: 12
+        });
+};
+
+function centrarMapa(position){
+    map.setZoom(18);
+    map.setCenter(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+        map: map,
+        icon: "image/bloggif_58a70bf41eac6.png"
+    });
+};
+
+function mostrarErrores(){
+
+}
+
+function init(){
+    if(navigator.geolocation){
+        console.log('soportado');
+        navigator.geolocation.watchPosition(centrarMapa);
+    }
+    else
+    {
+        console.log('no soportado');
+    }
+};
 
 
-var myLatLng = {
+/*var myLatLng = {
 	lat: -16.457389199999998, 
 	lng: -71.5315308
 };
@@ -39,5 +64,4 @@ var myLatLng1 = {
     map: map,
     label: '2',
 	icon: "image/bloggif_58a70bf41eac6.png"
-  });
-}
+  });*/
