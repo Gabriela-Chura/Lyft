06579dba03ba73b70b66d2d9364ee45521198e60
Daniela Alvarez Zegarra <daniela.alvarez04@gmail.com>
2017-02-28 12:55:49 -0500

    pruebas de mapas

--- JS/appdos.js
+++ JS/appdos.js
@@ -39,28 +39,3 @@ function init(){
         console.log('no soportado');
     }
 };
-
-
-/*var myLatLng = {
-	lat: -16.457389199999998, 
-	lng: -71.5315308
-};
-	
-var myLatLng1 = {
-	lat: -16.457389199992998, 
-	lng: -71.4315458
-};
-
- var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Aquí hellooo!',
-	icon: "image/bloggif_58a70bf41eac6.png"
-  });
-	
-	new google.maps.Marker({
-    position: myLatLng1,
-    map: map,
-    label: '2',
-	icon: "image/bloggif_58a70bf41eac6.png"
-  });*/
